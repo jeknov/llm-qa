a7a12fe63d154ceb6c1d32445e2cf2fc7baf161c
Jekaterina <novikova.jekaterina@gmail.com>
2024-05-03 21:40:48 -0400

    update as streamlit app

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-import os
+from langchain_community.llms import HuggingFaceHub
 
-from langchain.llms import HuggingFaceHub
-
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = HUGGINGFACEHUB_API_TOKEN
+import streamlit as st
 
 
-def llm_answer(model='google/flan-t5-large', query="What is the currency of Lithuania?"):
+def llm_answer(query):
+    model = 'meta-llama/Meta-Llama-3-8B-Instruct'
     llm = HuggingFaceHub(repo_id=model)
     completion = llm(query)
-    print("\n")
-    print(completion)
+    return completion
 
 
-if __name__ == "__main__":
-    llm_answer()
+st.set_page_config(page_title='LangChain Demo', page_icon=":robot:")
+st.header("LangChain Demo")
+
+
+def get_text():
+    input_text = st.text_input("You: ", key="input")
+    return input_text
+
+
+user_input = get_text()
+response = llm_answer(user_input)
+
+submit = st.button("Generate")
+
+if submit:
+    st.subheader("Answer: ")
+    st.write(response)
